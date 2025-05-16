fe6cdc8eeca1f93f1e4ebaae6b6060f9157380ea
Luke Miloszewski <luke.miloszewski@ninetyone.com>
2025-05-16 20:42:52 +0100

    Digital ocean

---
+++ scripts/2025_02_14-do-object-storage.py
@@ -0,0 +1,13 @@
+from boto3 import session
+
+
+session = session.Session()
+client = session.client(
+    's3',
+    region_name='lon1',
+    endpoint_url='https://lukemiloszewski.lon1.digitaloceanspaces.com',
+    aws_access_key_id=ACCESS_ID,
+    aws_secret_access_key=SECRET_KEY
+)
+
+client.upload_file('hello.html', 'lukemiloszewski', ...)
